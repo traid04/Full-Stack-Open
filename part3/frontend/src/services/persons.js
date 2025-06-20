b68329adc2fbe3e1c00f2359b11c1fd566033e01
Traid <camposmatihas@gmail.com>
2025-06-19 21:19:01 -0300

    Parte 3 completa

---
+++ part3/frontend/src/services/persons.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+const baseURL = '/api/persons';
+
+const getAll = () => {
+    const request = axios.get(baseURL);
+    return request.then(result => result.data);
+}
+
+const create = newPerson => {
+    const request = axios.post(baseURL, newPerson);
+    return request.then(result => result.data);
+}
+
+const update = updatedPerson => {
+    const request = axios.put(`${baseURL}/${updatedPerson.id}`, updatedPerson);
+    return request.then(result => result.data);
+}
+
+const remove = id => {
+    const request = axios.delete(`${baseURL}/${id}`);
+    return request.then(result => result.data);
+}
+
+export default {getAll, create, remove, update};
